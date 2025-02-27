742d8de7cd367cc2d39145fca9406d8a6d6e8a30
Asib <ahmedasib1@gmail.com>
2025-02-27 23:47:10 +0600

    feat #3 : how to use provider

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -20,9 +28,8 @@ export class UserController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return {
-      out:this.userService.findOne(+id),
+      out: this.userService.findOne(+id),
       id,
-
     };
   }
 
